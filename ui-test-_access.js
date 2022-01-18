c1bc74bc10763ca1bc8911b8b969cb1a5600ff44
Rosano Coutinho <pub@xeari.com>
2022-01-18 12:01:10 -0500

    Lint

--- ui-test-_access.js
+++ ui-test-_access.js
@@ -3,7 +3,7 @@ const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 Object.entries({
 	SWARLink: '.SWARLink',
 }).map(function (e) {
-	return global[e.shift()]  = e.pop();
+	return global[e.shift()] = e.pop();
 });
 
 describe('SWARLink_Access', function () {
